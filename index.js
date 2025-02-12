88a8b91521c920b7ae0af70c12e73121f25ecf96
dm-yeu <daniel.mendt@yaskawa.eu.com>
2025-02-12 15:47:11 +0100

    implemented infinite loop with exit conditions in main - enables user to perform multiple operations

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { fileSearch } from './empolis_search.js';
 import { editFileMetadata } from './empolis_ops.js';
 import { loadConfig, getHtmlFiles, readJsonData } from './helpers.js';
 import { createFileIndex } from './index_creation.js';
+import logger, { logPrettyJson } from './logger.js';
 import { performance } from 'node:perf_hooks';
 import { fileURLToPath } from 'url';
 import path from 'node:path';
 import dotenv from 'dotenv';
-import logger, { logPrettyJson } from './logger.js';
 import chalk from 'chalk';
 import { confirm, input } from '@inquirer/prompts';
 
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: __dirname + `/.env` });
 
 // Load configuration
-export const config = await loadConfig({ promptUser: true });
+export let config = await loadConfig({ promptUser: true });
 logger.debug(`config:\n${config}`);
 
 /**
@@ -41,31 +41,47 @@ async function main() {
   logger.info(`Run of empolis-visibility main() started`);
 
   try {
+    let newOperation = true;
     let fileList = [];
     let indexFile = '';
-    // Create index file for the data source if user selects 'index' or 'update' operation
-    if (config.OPERATION === 'index' || config.OPERATION === 'update') {
-      ({ fileList, indexFile } = await createUpdateIndexFile());
-    }
-    // Update metadata for each file in the index if user selects 'update' operation
-    if (config.OPERATION === 'update') {
-      await updateCloudMetadata({ fileList, indexFile });
-    }
-
-    if (config.OPERATION === 'file_search') {
-      const searchTerm = await input({ message: 'Enter the filename to search for:' });
-      const fileMetadata = await fileSearch({ searchTerm, consoleOutput: true });
-      logger.info(`fileMetadata:\n${JSON.stringify(fileMetadata)}`);
+    while (newOperation) {
+      // Create index file for the data source if user selects 'index' or 'update' operation
+      if (config.OPERATION === 'index' || config.OPERATION === 'update') {
+        ({ fileList, indexFile } = await createUpdateIndexFile());
+      }
+      // Update metadata for each file in the index if user selects 'update' operation
+      if (config.OPERATION === 'update') {
+        await updateCloudMetadata({ fileList, indexFile });
+      }
+      // Search for a specific file in the data source if user selects 'file_search' operation
+      if (config.OPERATION === 'file_search') {
+        const searchTerm = await input({ message: 'Enter the filename to search for:' });
+        const fileMetadata = await fileSearch({ searchTerm, consoleOutput: true });
+        logger.info(`fileMetadata:\n${JSON.stringify(fileMetadata)}`);
+      }
+      // Prompt user to perform another operation
+      newOperation = await confirm(
+        { message: 'Perform another operation', default: false },
+        { signal: AbortSignal.timeout(10000) }
+      );
+      if (newOperation) {
+        config = await loadConfig({ promptUser: true });
+      }
     }
   } catch (error) {
-    console.error(`main() Error:\n${error}`);
-    logger.error(`main() Error:\n${error}`);
+    if (error.name === 'AbortPromptError') {
+      console.log(`${chalk.red('X')} Timeout. Operation cancelled due to inactivity`);
+      logger.info(`Timeout. Operation cancelled due to inactivity`);
+    } else {
+      console.error(`main() Error:\n${error}`);
+      logger.error(`main() Error:\n${error}`);
+    }
   } finally {
     const endTime = performance.now();
     const executionTime = (endTime - startTime) / 1000; // Convert ms to seconds
-    logger.info(`Done. Execution time: ${executionTime.toFixed(2)} seconds`);
+    logger.info(`Total execution time: ${executionTime.toFixed(2)} seconds`);
     console.log(
-      `Done - execution time: ${chalk.cyan(executionTime.toFixed(2))} seconds.` +
+      `Total execution time: ${chalk.cyan(executionTime.toFixed(2))} seconds.` +
         ` Logs can be found in ${chalk.cyan(logger.transports[0].dirname)}.`
     );
   }
@@ -177,7 +193,7 @@ async function processFile(authToken, dataObject) {
   logger.info(`Processing ${dataObject.filename}`);
   logger.debug(`dataObject: ${JSON.stringify(dataObject)}`);
 
-  // Search for file in the Empolis index to get the current metadata
+  // Search for file in the user selected data source to get the current metadata
   const fileMetadata = await fileSearch({ searchTerm: dataObject.filename, consoleOutput: false });
 
   let newKeywords = '';
@@ -211,7 +227,7 @@ async function processFile(authToken, dataObject) {
     }
   }
 
-  // Build new metadata object with Title and Keywords (optional)
+  // Build new metadata object with Title and optional Keywords
   let newMetadata = { ...fileMetadata, Title: dataObject.title };
   if (newKeywords.length > 0) {
     newMetadata = { ...newMetadata, Keywords_txt: newKeywords };
